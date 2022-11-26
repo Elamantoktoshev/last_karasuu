4a559bfe487bb4904ebb64a9f38494e6282297fd
elaman <73353291+Elamantoktoshev@users.noreply.github.com>
2022-11-26 12:57:28 +0600

    Update settings.py

--- karasuu/settings.py
+++ karasuu/settings.py
@@ -141,8 +141,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+# STATIC SETTING
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# END STATIC SETTING
+
+
+# MEDIA SETTING
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+# END MEDIA SETTING
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
