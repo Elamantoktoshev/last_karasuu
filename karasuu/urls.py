522f8cfb01f71c34c56dd4d1fd1a6d53ed510bf3
elaman <73353291+Elamantoktoshev@users.noreply.github.com>
2022-11-26 12:59:36 +0600

    Update urls.py

--- karasuu/urls.py
+++ karasuu/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path
 # from django.urls import url
 from ads_api import views
+from config import settings
+from django.conf.urls.static import static
+
+
 
 
 urlpatterns = [
@@ -25,3 +29,6 @@ urlpatterns = [
     path('list/', views.AdsApiView.as_view())
 
 ]
+if settings.DEBUG:
+    '''Если это тестовый режим'''
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
